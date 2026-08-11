50c9b3f99c25550e26bc530d9d2dd714eb148df7
fiqri nur faqih muhammad <msgfiqri17@gmail.com>
2026-08-11 14:28:33 +0700

    Update API domain to use HTTPS to fix Mixed Content

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create Axios instance with credentials
 export const api = axios.create({
-  baseURL: 'http://kliniksejahterah.gt.tc/backend/api',
+  baseURL: 'https://kliniksejahterah.gt.tc/backend/api',
   withCredentials: true
 });
 
